aaa53507f2035b6331dd2948c65466833a30fab9
SanderBorgmans <sanderborgmans@gmail.com>
2021-01-14 16:24:26 +0100

    Added route section from log file, corrected for multi line (FCHK file route section could also be used if FCHKFile is updated)

--- pyiron/gaussian/gaussian.py
+++ pyiron/gaussian/gaussian.py
@@ -847,24 +847,30 @@ def read_EmpiricalDispersion(output_file,output_dict):
     # Get dispersion term from log file if it is there
     # dispersion term is not retrieved from gaussian output in fchk
 
-    disp = None
-    with open(output_file,'r') as f:
-        while True:
-            line = f.readline()
-            if 'Route' in line:
-                line = f.readline()
-                if 'EmpiricalDispersion' in line:
-                    idx = line.find('EmpiricalDispersion')
-                    if 'GD3' in line[idx:]:
-                        search_term = 'Grimme-D3 Dispersion energy='
-                    else:
-                        raise NotImplementedError
-                else:
-                    return
-                break
-
     # the log file has the same path and name as the output file aside from the file extension
     log_file = output_file[:output_file.rfind('.')] + '.log'
+    route = ''
+
+    disp = None
+    with open(log_file,'r') as f:
+        while True:
+            line = f.readline()
+            if line.startswith('#'):
+                while not (line.startswith('-')):
+                    route += line
+                    line = f.readline().lstrip(' ')
+            route = route.replace("\n","").lower()
+            break
+
+    if 'empiricaldispersion' in route:
+        idx = line.find('empiricaldispersion')
+        if 'gd3' in line[idx:]:
+            search_term = 'Grimme-D3 Dispersion energy='
+        else:
+            raise NotImplementedError('Could not read the kind of EmpiricalDispersion being used.')
+    else:
+         return
+
     it = _reverse_readline(log_file)
     while True:
         line = next(it)
